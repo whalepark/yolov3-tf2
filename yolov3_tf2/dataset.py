2a2f0d7b5bb395dc882c79b4486c4f8757dab49f
Misun Park <misun@gatech.edu>
2020-04-14 04:22:46 +0000

    remove tf
    Signed-off-by: Misun Park <misun@gatech.edu>

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -8,46 +8,46 @@ sys.path.insert(0, os.path.abspath('../tfrpc/client'))
 from tf_wrapper import TFWrapper, YoloWrapper
 os.chdir(cwd)
 
-@tf.function
-def transform_targets_for_output(y_true, grid_size, anchor_idxs):
-    # y_true: (N, boxes, (x1, y1, x2, y2, class, best_anchor))
-    N = tf.shape(y_true)[0]
+# @tf.function
+# def transform_targets_for_output(y_true, grid_size, anchor_idxs):
+#     # y_true: (N, boxes, (x1, y1, x2, y2, class, best_anchor))
+#     N = tf.shape(y_true)[0]
 
-    # y_true_out: (N, grid, grid, anchors, [x, y, w, h, obj, class])
-    y_true_out = tf.zeros(
-        (N, grid_size, grid_size, tf.shape(anchor_idxs)[0], 6))
+#     # y_true_out: (N, grid, grid, anchors, [x, y, w, h, obj, class])
+#     y_true_out = tf.zeros(
+#         (N, grid_size, grid_size, tf.shape(anchor_idxs)[0], 6))
 
-    anchor_idxs = tf.cast(anchor_idxs, tf.int32)
+#     anchor_idxs = tf.cast(anchor_idxs, tf.int32)
 
-    indexes = tf.TensorArray(tf.int32, 1, dynamic_size=True)
-    updates = tf.TensorArray(tf.float32, 1, dynamic_size=True)
-    idx = 0
-    for i in tf.range(N):
-        for j in tf.range(tf.shape(y_true)[1]):
-            if tf.equal(y_true[i][j][2], 0):
-                continue
-            anchor_eq = tf.equal(
-                anchor_idxs, tf.cast(y_true[i][j][5], tf.int32))
+#     indexes = tf.TensorArray(tf.int32, 1, dynamic_size=True)
+#     updates = tf.TensorArray(tf.float32, 1, dynamic_size=True)
+#     idx = 0
+#     for i in tf.range(N):
+#         for j in tf.range(tf.shape(y_true)[1]):
+#             if tf.equal(y_true[i][j][2], 0):
+#                 continue
+#             anchor_eq = tf.equal(
+#                 anchor_idxs, tf.cast(y_true[i][j][5], tf.int32))
 
-            if tf.reduce_any(anchor_eq):
-                box = y_true[i][j][0:4]
-                box_xy = (y_true[i][j][0:2] + y_true[i][j][2:4]) / 2
+#             if tf.reduce_any(anchor_eq):
+#                 box = y_true[i][j][0:4]
+#                 box_xy = (y_true[i][j][0:2] + y_true[i][j][2:4]) / 2
 
-                anchor_idx = tf.cast(tf.where(anchor_eq), tf.int32)
-                grid_xy = tf.cast(box_xy // (1/grid_size), tf.int32)
+#                 anchor_idx = tf.cast(tf.where(anchor_eq), tf.int32)
+#                 grid_xy = tf.cast(box_xy // (1/grid_size), tf.int32)
 
-                # grid[y][x][anchor] = (tx, ty, bw, bh, obj, class)
-                indexes = indexes.write(
-                    idx, [i, grid_xy[1], grid_xy[0], anchor_idx[0][0]])
-                updates = updates.write(
-                    idx, [box[0], box[1], box[2], box[3], 1, y_true[i][j][4]])
-                idx += 1
+#                 # grid[y][x][anchor] = (tx, ty, bw, bh, obj, class)
+#                 indexes = indexes.write(
+#                     idx, [i, grid_xy[1], grid_xy[0], anchor_idx[0][0]])
+#                 updates = updates.write(
+#                     idx, [box[0], box[1], box[2], box[3], 1, y_true[i][j][4]])
+#                 idx += 1
 
-    # tf.print(indexes.stack())
-    # tf.print(updates.stack())
+#     # tf.print(indexes.stack())
+#     # tf.print(updates.stack())
 
-    return tf.tensor_scatter_nd_update(
-        y_true_out, indexes.stack(), updates.stack())
+#     return tf.tensor_scatter_nd_update(
+#         y_true_out, indexes.stack(), updates.stack())
 
 
 def transform_targets(y_train, anchors, anchor_masks, size):
