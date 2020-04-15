3dba935e36051aeb8d8146fc0f8815c9b590222a
Misun Park <misun@gatech.edu>
2020-04-15 05:09:33 +0000

    dd
    Signed-off-by: Misun Park <misun@gatech.edu>

--- tfrpc/proto/gen_proto.sh
+++ tfrpc/proto/gen_proto.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+rm -f ../client/{yolo_pb2_grpc.py,yolo_pb2.py}
+rm -f ../server/{yolo_pb2_grpc.py,yolo_pb2.py}
+
 python3 -m grpc_tools.protoc -I. --python_out=../server --grpc_python_out=../server yolo.proto
 python3 -m grpc_tools.protoc -I. --python_out=../client --grpc_python_out=../client yolo.proto
