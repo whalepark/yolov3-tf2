cfcb064d181edd85d0bfb8467c6b931df1bda76d
Misun Park <misun@gatech.edu>
2020-04-15 06:27:25 +0000

    dd

--- tfrpc/server/yolo_server.py
+++ tfrpc/server/yolo_server.py
@@ -533,6 +533,7 @@ class YoloFunctionWrapper(yolo_pb2_grpc.YoloTensorflowWrapperServicer):
         print(f'misun: unpickled_iterable={type(unpickled_iterable)}, len={len(unpickled_iterable)}')
         print(f'misun: unpickled_iterable[0]={type(unpickled_iterable[0])}, len={len(unpickled_iterable[0])}')
         print(f'misun: unpickled_unpickled_iterable[0]={type(pickle.loads(unpickled_iterable[0]))}, len={len(pickle.loads(unpickled_iterable[0]))}')
+        print(f'misun: (unpickled_(unpickled_iterable[0]))[0]={type(pickle.loads(unpickled_iterable[0])[0])}, len={len(pickle.loads(unpickled_iterable[0])[0])}')
 
         index = request.index
 
