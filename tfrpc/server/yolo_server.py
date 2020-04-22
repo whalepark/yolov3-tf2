3238c453983ebc65327a6f285151e972d8f4a19d
Misun Park <misun@gatech.edu>
2020-04-22 21:38:08 +0000

    whalepark
    Signed-off-by: Misun Park <misun@gatech.edu>

--- tfrpc/server/yolo_server.py
+++ tfrpc/server/yolo_server.py
@@ -644,9 +644,10 @@ class YoloFunctionWrapper(yolo_pb2_grpc.YoloTensorflowWrapperServicer):
                 ref_val = ref_val[index]
 
             print(f'misun: type={type(ref_val)}')
-            print(f'misun: ref_val.shape={ref_val.shape}')
+            # print(f'misun: ref_val.shape={ref_val.shape}')
             print(f'misun: attribute={ref_val.__dir__()}')
             print(f'misun: ref_val={ref_val}')
+            print(f'misun: try pickle={pickle.dumps(ref_val)}')
 
             try:
                 for elem in ref_val:
