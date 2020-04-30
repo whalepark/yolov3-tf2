0bc225b7d76b18f68610002f57a21c6fd56cf517
Misun Park <misun@gatech.edu>
2020-04-30 04:07:26 +0000

    cpp version server start
    Signed-off-by: Misun Park <misun@gatech.edu>

--- tfrpc/proto/gen_proto.sh
+++ tfrpc/proto/gen_proto.sh
@@ -5,3 +5,12 @@ rm -f ../server/{yolo_pb2_grpc.py,yolo_pb2.py}
 
 python3 -m grpc_tools.protoc -I. --python_out=../server --grpc_python_out=../server yolo.proto
 python3 -m grpc_tools.protoc -I. --python_out=../client --grpc_python_out=../client yolo.proto
+
+SRC_DIR=.
+SERVER_DST_DIR=../server
+
+protoc -I=$SRC_DIR --cpp_out=$SERVER_DST_DIR $SRC_DIR/yolo.proto
+protoc -I=$SRC_DIR --grpc_out=$SERVER_DST_DIR --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $SRC_DIR/yolo.proto
+
+cd ../server
+make
