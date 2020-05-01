499a327a83a4a4adf42067ac4a485d667db884e4
Misun Park <misun@gatech.edu>
2020-05-01 01:39:46 +0000

    dd
    Signed-off-by: Misun Park <misun@gatech.edu>

--- tfrpc/server/yolo_server.cpp
+++ tfrpc/server/yolo_server.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // #include <atomic>
-// #include <thread> 
+// #include <thread>
 
 // #include <tensorflow/c/c_api.h>
 
@@ -19,16 +19,12 @@
 // using yolo_tf::HelloRequest;
 // using yolo_tf::YoloTensorflowWrapper;
 
-
-
-
 // Global Variables
 // Global_Tensor_Dict = {}
 // Object_Ownership = {}
 // Connection_Set = set()
 // Global_Graph_Dict = {}
 // Global_Sess_Dict = {}
-
 
 // std::atomic<int> conv2d_count(0);
 // std::atomic<int> batch_norm_count(0);
@@ -65,151 +61,151 @@
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status callable_emulator(ServerContext *context, const CallRequest *request, CallResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status get_iterable_slicing(ServerContext *context, const SlicingRequest *request, SlicingResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status constant(ServerContext *context, const ConstantRequest *request, ConstantResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status config_experimental_list__physical__devices(ServerContext *context, const DeviceType *request, PhysicalDevices *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status image_decode__image(ServerContext *context, const DecodeImageRequest *request, DecodeImageResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status expand__dims(ServerContext *context, const ExpandDemensionRequest *request, ExpandDemensionResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_layers_Input(ServerContext *context, const InputRequest *request, InputResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_Model(ServerContext *context, const ModelRequest *request, ModelResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_layers_ZeroPadding2D(ServerContext *context, const ZeroPadding2DRequest *request, ZeroPadding2DResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_layers_Conv2D(ServerContext *context, const Conv2DRequest *request, Conv2DResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_layers_LeakyReLU(ServerContext *context, const LeakyReluRequest *request, LeakyReluResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_layers_Add(ServerContext *context, const AddRequest *request, AddResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_layers_Lambda(ServerContext *context, const LambdaRequest *request, LambdaResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_layers_UpSampling2D(ServerContext *context, const UpSampling2DRequest *request, UpSampling2DResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_layers_Concatenate(ServerContext *context, const ConcatenateRequest *request, ContcatenateResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status image_resize(ServerContext *context, const ImageResizeRequest *request, ImageResizeResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status keras_regularizers_l2(ServerContext *context, const l2Request *request, l2Response *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status attribute_tensor_shape(ServerContext *context, const TensorShapeRequest *request, TensorShapeResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status attribute_model_load__weight(ServerContext *context, const LoadWeightsRequest *request, LoadWeightsResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status attribute_checkpoint_expect__partial(ServerContext *context, const ExpectPartialRequest *request, ExpectPartialResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status tensor_op_divide(ServerContext *context, const DivideRequest *request, DivideResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status iterable_indexing(ServerContext *context, const IndexingRequest *request, IndexingResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status byte_tensor_to_numpy(ServerContext *context, const TensorToNumpyRequest *request, TensorToNumPyResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status get_object_by_id(ServerContext *context, const GetObjectRequest *request, GetObjectResponse *response) override
 //     // {
 //     //     reply->set_message();
 //     //     return Status::OK;
 //     // }
-    
+
 //     // Status batch_normalization(ServerContext *context, const BatchNormRequest *request, BatchNormResponse *response) override
 //     // {
 //     //     reply->set_message();
@@ -239,7 +235,7 @@
 //     server->Wait();
 // }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     printf("hello world!");
     // printf("Hello from TensorFlow C library version %s\n", TF_Version());
