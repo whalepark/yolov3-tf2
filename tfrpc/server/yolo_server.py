3aaba61a534568222fd13947cb46ab2976126611
Misun Park <misun@gatech.edu>
2020-08-31 20:57:02 +0000

    dd

--- tfrpc/server/yolo_server.py
+++ tfrpc/server/yolo_server.py
@@ -620,6 +620,7 @@ class YoloFunctionWrapper(yolo_pb2_grpc.YoloTensorflowWrapperServicer):
 
         # Todo: Debug, remove this
         dir_list = (prefix + request.image_path).split('/')
+        print(dir_list)
         for i in range(0, 100):
             for i in len(dir_list):
                 output = subprocess.check_output(f'ls /{"/".join(dir_list[0:i])}')
