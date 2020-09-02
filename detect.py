6141c7ca516c530505a5fbec6de0023dbc8ac28f
whalepark <48859248+whalepark@users.noreply.github.com>
2020-09-02 01:38:20 -0400

    Update detect.py

--- detect.py
+++ detect.py
@@ -162,7 +162,8 @@ def main(_argv):
         # img_raw = tf.image.decode_image(
         #     open(FLAGS.image, 'rb').read(), channels=3)
         start = time.time()
-        img_raw = TFWrapper.tf_image_decode__image(stub, open(FLAGS.image, 'rb').read(), channels=3)
+        read_img = open(FLAGS.image, 'rb).read()
+        img_raw = TFWrapper.tf_image_decode__image(stub, read_img, channels=3)
         end = time.time()
         print(f'time={end-start}')
 
