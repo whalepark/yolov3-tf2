a9228e005da89a599ee645d578c2fe8791c6eb34
whalepark <48859248+whalepark@users.noreply.github.com>
2020-09-02 00:52:03 -0400

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,5 +1,32 @@
+
+def make_json(container_id):
+    import json
+    args_dict = {}
+
+    args_dict['type']='closed-proc-ns'
+    args_dict['cid']=container_id
+    args_dict['events']=['cpu-cycles','page-faults','minor-faults','major-faults','cache-misses','LLC-load-misses','LLC-store-misses','dTLB-load-misses','iTLB-load-misses']
+
+    args_json = json.dumps(args_dict)
+
+    return args_json
+
+def connect_to_perf_server():
+    import socket
+    PERF_SERVER_SOCKET = '/sockets/perf_server.sock'
+    container_id = socket.gethostname()
+    my_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    my_socket.connect(PERF_SERVER_SOCKET)
+    json_data_to_send = make_json(container_id)
+    my_socket.sendall(json_data_to_send.encode('utf-8'))
+    data_received = my_socket.recv(1024)
+    print(data_received)
+    my_socket.close()
+
+connect_to_perf_server()
+
+
 import time, subprocess
-import socket # to notify perf server
 import shlex # for subprocess popen
 from multiprocessing import Process
 from absl import app, flags, logging
