a01427151aabf9ea61d4d7f115a33f1fe31a062d
whalepark <48859248+whalepark@users.noreply.github.com>
2021-05-06 02:44:26 -0400

    warmup added.

--- detect.py
+++ detect.py
@@ -20,53 +20,60 @@ flags.DEFINE_string('tfrecord', None, 'tfrecord instead of image')
 flags.DEFINE_string('output', './output.jpg', 'path to output image')
 flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
 
+g_yolo = None
+
 
 def main(_argv):
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    if len(physical_devices) > 0:
-        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    for i in (1, 2):
+        physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        if len(physical_devices) > 0:
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-    t1 = time.time()
-    if FLAGS.tiny:
-        yolo = YoloV3Tiny(classes=FLAGS.num_classes)
-    else:
-        yolo = YoloV3(classes=FLAGS.num_classes)
+        t1 = time.time()
+        if FLAGS.tiny:
+            yolo = YoloV3Tiny(classes=FLAGS.num_classes)
+        else:
+            if g_yolo is not None:
+                yolo = g_yolo
+            else:
+                yolo = YoloV3(classes=FLAGS.num_classes)
+                g_yolo = yolo
 
-    yolo.load_weights(FLAGS.weights).expect_partial()
-    t2 = time.time()
-    logging.info('weights loaded')
-    logging.info(f'graph_construction_time: {t2-t1}')
+        yolo.load_weights(FLAGS.weights).expect_partial()
+        t2 = time.time()
+        logging.info('weights loaded')
+        logging.info(f'graph_construction_time_{i} {t2-t1}')
 
-    class_names = [c.strip() for c in open(FLAGS.classes).readlines()]
-    logging.info('classes loaded')
+        class_names = [c.strip() for c in open(FLAGS.classes).readlines()]
+        logging.info('classes loaded')
 
-    if FLAGS.tfrecord:
-        dataset = load_tfrecord_dataset(
-            FLAGS.tfrecord, FLAGS.classes, FLAGS.size)
-        dataset = dataset.shuffle(512)
-        img_raw, _label = next(iter(dataset.take(1)))
-    else:
-        img_raw = tf.image.decode_image(
-            open(FLAGS.image, 'rb').read(), channels=3)
+        if FLAGS.tfrecord:
+            dataset = load_tfrecord_dataset(
+                FLAGS.tfrecord, FLAGS.classes, FLAGS.size)
+            dataset = dataset.shuffle(512)
+            img_raw, _label = next(iter(dataset.take(1)))
+        else:
+            img_raw = tf.image.decode_image(
+                open(FLAGS.image, 'rb').read(), channels=3)
 
-    img = tf.expand_dims(img_raw, 0)
-    img = transform_images(img, FLAGS.size)
+        img = tf.expand_dims(img_raw, 0)
+        img = transform_images(img, FLAGS.size)
 
-    t1 = time.time()
-    boxes, scores, classes, nums = yolo(img)
-    t2 = time.time()
-    logging.info('inference_time: {}'.format(t2 - t1))
+        t1 = time.time()
+        boxes, scores, classes, nums = yolo(img)
+        t2 = time.time()
+        logging.info('inference_time_{} {}'.format(i, t2 - t1))
 
-    logging.info('detections:')
-    for i in range(nums[0]):
-        logging.info('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
-                                           np.array(scores[0][i]),
-                                           np.array(boxes[0][i])))
+        logging.info('detections:')
+        for i in range(nums[0]):
+            logging.info('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
+                                               np.array(scores[0][i]),
+                                               np.array(boxes[0][i])))
 
-    img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
-    img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
-    cv2.imwrite(FLAGS.output, img)
-    logging.info('output saved to: {}'.format(FLAGS.output))
+        img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
+        img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
+        cv2.imwrite(FLAGS.output, img)
+        logging.info('output saved to: {}'.format(FLAGS.output))
 
 
 if __name__ == '__main__':
